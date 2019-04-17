ff3ae5927000a8a379ab28c26c9df48d8b49d7da
tsimete <tsimete@beckman.com>
2019-04-17 23:50:42 +0200

    Add missing unit tests

---
+++ frontend/src/app/app.settings.spec.ts
@@ -0,0 +1,59 @@
+import { TestBed, async } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { AppSettingsHttpService, AppSettingsService, AppSettings } from './app.settings';
+import { APP_BASE_HREF } from '@angular/common';
+import * as Observable from 'rxjs';
+
+describe('AppSettings', () => {
+  const appSettings: AppSettings = new AppSettings();
+  let appSettingsService: AppSettingsService = new AppSettingsService();
+  let appSettingsHttpService: AppSettingsHttpService;
+  let httpClientSpy: {
+    get: jasmine.Spy,
+  };
+  beforeEach(async(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    appSettings.categoriesApiUrl = 'http://localhost:4000/categories';
+
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+      ],
+      providers: [
+        AppSettingsService,
+        AppSettingsHttpService,
+        { provide: HttpClient, useValue: httpClientSpy },
+        { provide: AppSettingsService, useValue: { settings: { categoriesApiUrl: 'http://localhost:4000/categories' } } },
+        { provide: APP_BASE_HREF, useValue: '/' }
+      ],
+    });
+    appSettingsHttpService = TestBed.get(AppSettingsHttpService);
+    appSettingsService = TestBed.get(AppSettingsService);
+  }));
+
+  it('should create the AppSettingsService', () => {
+    expect(appSettingsService).toBeTruthy();
+  });
+
+  it('should create the AppSettingsHttpService', () => {
+    expect(appSettingsHttpService).toBeTruthy();
+  });
+
+  describe('on app initialization', () => {
+    beforeEach(async(() => {
+      httpClientSpy.get.and.returnValues(Observable.of<AppSettings>(appSettings));
+      appSettingsHttpService.initializeApp();
+    }));
+
+    it('should get data from assets/settings.json', () => {
+      expect(httpClientSpy.get).toHaveBeenCalled();
+      expect(httpClientSpy.get.calls.count()).toBe(1);
+      expect(httpClientSpy.get.calls.first().args.length).toBe(1);
+      expect(httpClientSpy.get.calls.first().args[0]).toBe('assets/settings.json');
+    });
+
+    it('should assign categories url to AppSettingsService', () => {
+      expect(appSettingsService.settings).toBe(appSettings);
+    });
+  });
+});

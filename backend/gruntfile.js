b94bb1674a683951b8d59b8a65db835995c63c59
tsimete <tsimete@beckman.com>
2019-04-18 10:25:55 +0200

    Fix coverage on backend to typescript coverage

--- backend/gruntfile.js
+++ backend/gruntfile.js
@@ -29,33 +29,14 @@ module.exports = function (grunt) {
                 tasks: ["ts", "tslint"]
             }
         },
-        coveralls: {
-            // Options relevant to all targets
-            options: {
-              // When true, grunt-coveralls will only print a warning rather than
-              // an error, to prevent CI builds from failing unnecessarily (e.g. if
-              // coveralls.io is down). Optional, defaults to false.
-              force: false
-            },
-        
-            backend: {
-              // LCOV coverage file (can be string, glob or array)
-              src: 'coverage/*.info',
-              options: {
-                // Any options for just this target
-              }
-            },
-          },
     });
 
     grunt.loadNpmTasks("grunt-contrib-watch");
     grunt.loadNpmTasks("grunt-ts");
     grunt.loadNpmTasks("grunt-tslint");
-    grunt.loadNpmTasks('grunt-coveralls');
 
     grunt.registerTask("default", [
         "ts",
         "tslint"
     ]);
-
 };

30ef60f96c8828fccdafede1c3f0fb319b9e0a1e
tsimete <tsimete@beckman.com>
2022-11-17 12:08:08 +0100

    Fix rebuild.cmd

--- rebuild.cmd
+++ rebuild.cmd
@@ -1,14 +1,13 @@
 cd frontend
-npm ci --legacy-peer-deps
-npm install coveralls
-ng build --configuration production
-ng test --watch false --browsers ChromeHeadless --code-coverage
-codecov --disable=gcov
-npx ng lint frontend --format json --output-file eslint.json
-cd ../backend
-npm ci --legacy-peer-deps
-npm install coveralls
-npm run grunt ts
-npm test
-codecov --disable=gcov 
-npm run grunt eslint -- --format=json --output-file=eslint.json
+call npm ci --legacy-peer-deps 
+call npm install codecov snyk @angular/cli -g
+call ng build --configuration production
+call ng test --watch false --browsers ChromeHeadless --code-coverage
+call codecov --disable=gcov
+call npx ng lint frontend --format json --output-file eslint.json
+cd ..\backend
+call npm ci --legacy-peer-deps
+call npm run grunt ts
+call npm test
+call codecov --disable=gcov 
+call npm run grunt eslint -- --format=json --output-file=eslint.json

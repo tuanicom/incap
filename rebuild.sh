71d27ac5ce139ff86b070aec5cc765a6158af4f1
tsimete <tsimete@beckman.com>
2023-11-21 21:38:53 +0100

    rework rebuild scripts

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+set -e
 if [ $# -eq 0 ] || [ "$1" = "frontend" ]; then
     cd frontend
     npm ci 
-    ng build --configuration production
-    ng test --watch false --browsers ChromeHeadless --code-coverage
+    ng build --configuration production 
+    ng test --watch false --browsers ChromeHeadless --code-coverage 
     npx ng lint frontend --format json --output-file eslint.json
     cd ..
 fi
@@ -11,8 +12,8 @@ if [ $# -eq 0 ] ||  [ "$1" = "backend" ];
     then 
     cd backend
     npm ci 
-    npm run grunt ts
-    npm test
+    npm run grunt ts 
+    npm test 
     npm run grunt eslint -- --format=json --output-file=eslint.json
     cd ..
 fi

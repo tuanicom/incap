d64886ea6cacfdcddf57ba1142066ce4b871ff5d
Tuani SIMETE <tuani.simete@quarksup.com>
2026-02-08 01:40:16 +0100

    fix frontend lcov

---
+++ frontend/vitest.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    include: ['**/*.spec.ts', '**/*.spec.tsx', 'src/**/*.spec.ts'],
+    globals: true,
+    threads: false,
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      all: true,
+      src: ['src'],
+      reportsDirectory: 'coverage',
+      exclude: [
+        'node_modules',
+        'dist',
+        'coverage',
+        'tests',
+        'tests-results',
+        '**/*.js'
+      ]
+    }
+  }
+});
